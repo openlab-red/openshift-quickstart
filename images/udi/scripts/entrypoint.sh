69b4abfe342a164477ce6cd2a45981fa6042978d
Mattia Mascia <mattiam7@gmail.com>
2025-03-28 13:42:23 +0000

    refactor: devfile

--- images/udi/scripts/entrypoint.sh
+++ images/udi/scripts/entrypoint.sh
@@ -91,4 +91,18 @@ if [ $HOME_USER_MOUNTED -eq 0 ] && [ ! -f $STOW_COMPLETE ]; then
     touch $STOW_COMPLETE
 fi
 
+STOW_COMPLETE=/home/user/.stow_zsh_completed
+if [ $HOME_USER_MOUNTED -eq 0 ] && [ ! -f $STOW_COMPLETE ]; then
+
+    if [ ! -f /home/user/.oh-my-zsh/oh-my-zsh.sh ]; then
+        mkdir -p /home/user/.oh-my-zsh
+        echo "Executing stow for oh-my-zsh configuration" | tee -a /tmp/entrypoint.log
+        stow . -t /home/user/.oh-my-zsh -d /home/tooling/.oh-my-zsh --no-folding -v 2 > /tmp/stow-zsh.log 2>&1
+        echo "Stow operation completed for oh-my-zsh configuration. Logs available at /tmp/stow-zsh.log" | tee -a /tmp/entrypoint.log
+    fi
+
+    touch $STOW_COMPLETE
+    echo "Stow zsh operations completed and stow completion marker created." | tee -a /tmp/entrypoint.log
+fi
+
 exec "$@"
